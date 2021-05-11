9b6d3086a084ce3a0d2684761f8d1fc23b4372fb
agray998 <a.gray.4797@gmail.com>
2021-05-11 10:24:34 +0000

    Added status field

--- application/models.py
+++ application/models.py
@@ -2,11 +2,12 @@ from application import db
 
 class Questions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(100))
+    question = db.Column(db.String(50))
     options = db.relationship('Options', backref='question')
 
 class Options(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     optletter = db.Column(db.Char())
     option = db.Column(db.String(30))
+    status = db.Column(db.String(10))
     question = db.Column(db.Integer, db.ForeignKey('question.id'))
