80cbefc5a733c125d66b62ff6af3706d8da28adb
agray998 <a.gray.4797@gmail.com>
2021-05-13 10:05:37 +0000

    added get to apt commands in test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install python3
-sudo apt install python3-venv
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
