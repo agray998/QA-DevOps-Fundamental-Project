42e04c4442e2c54c463f053cd552a576117c577b
agray998 <a.gray.4797@gmail.com>
2021-05-16 14:20:47 +0000

    exported cov report as html

--- test.sh
+++ test.sh
@@ -11,4 +11,4 @@ sudo apt-get install python3-pip python3-venv -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=application --cov-report term-missing
+python3 -m pytest --cov=application --cov-report html
