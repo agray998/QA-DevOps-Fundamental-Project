2cbf7c10b440058f1aaee93512868ccc3f4e93d4
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2022-03-14 13:58:08 +0000

    Update and rename test.sh to test_basic.sh

---
+++ test_basic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install python3-pip python3-venv -y
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3 -m pytest --cov=application --cov-report html
