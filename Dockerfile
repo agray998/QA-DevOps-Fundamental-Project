8c8201237926e298f8d6570785bcd797255e4dc3
agray998 <a.gray.4797@gmail.com>
2022-03-14 16:09:17 +0000

    added dockerfile/dockerignore, fixed issues with pip installs

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.9
+WORKDIR /app
+COPY . .
+RUN pip3 install -r requirements.txt
+RUN python3 create.py
+EXPOSE 5000
+ENTRYPOINT ["python3", "app.py"]
--- application/__init__.py
+++ application/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('secretkey')
+app.config['SECRET_KEY'] = os.getenv('secret_key')
 
 db = SQLAlchemy(app)
 
