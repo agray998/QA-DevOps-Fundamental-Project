cecbf2407dc89dc3342523b5eb20d92d41a00d8f
agray998 <a.gray.4797@gmail.com>
2022-03-21 11:06:33 +0000

    sample text

--- flask-app/test_basic.sh
+++ flask-app/test_basic.sh
@@ -3,4 +3,5 @@ sudo apt install python3-pip python3-venv -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+echo $(pwd)
 python3 -m pytest --cov=application --cov-report html
